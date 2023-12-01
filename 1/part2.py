23b221a322d5fd564017d54467cd1bf12e0c9a55
Wes Barnett <wes@barnettphd.com>
2023-12-01 12:28:19 -0500

    update

--- 1/part2.py
+++ 1/part2.py
@@ -1,24 +1,18 @@
 from aoc import get_input, submit
 
-def get_num(line, num_spelled, reverse=False):
+def get_num(line, reverse=False):
     range_ = range(len(line)-1, -1, -1) if reverse else range(len(line))
     for i in range_:
         try:
             x = int(line[i])
             return x
-        except:
-            for j, ns in enumerate(num_spelled):
-                if line[i:].startswith(ns):
-                    return j+1
+        except ValueError:
+            num_spelled = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+            for j, x in enumerate(num_spelled, start=1):
+                if line[i:].startswith(x):
+                    return j
 
 if __name__ == "__main__":
     year, day, level = 2023, 1, 2
-    aoc_input = get_input(year, day)
-
-    lines = aoc_input.split("\n")
-    num_spelled= ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    result = sum(
-        get_num(line, num_spelled)*10 + get_num(line, num_spelled, reverse=True)
-        for line in lines
-    )
+    result = sum(get_num(line)*10 + get_num(line, reverse=True) for line in get_input(year, day).split("\n"))
     submit(result, year, day, level)
