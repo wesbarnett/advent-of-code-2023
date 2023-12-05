137083461e48e372b5043a8a5099430886a1060a
Wes Barnett <wes@barnettphd.com>
2023-12-05 07:05:17 -0500

    submit

--- 5/part1.py
+++ 5/part1.py
@@ -3,39 +3,6 @@ from aoc import get_input, submit
 if __name__ == "__main__":
     year, day, level = 2023, 5, 1
     aoc_input = get_input(year, day)
-    aoc_input = """seeds: 79 14 55 13
-
-seed-to-soil map:
-50 98 2
-52 50 48
-
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
-
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-
-water-to-light map:
-88 18 7
-18 25 70
-
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
-
-temperature-to-humidity map:
-0 69 1
-1 0 69
-
-humidity-to-location map:
-60 56 37
-56 93 4"""
 
     sections = aoc_input.split("\n\n")
     inputs = [int(x) for x in sections[0].partition(": ")[-1].split()]
@@ -61,5 +28,4 @@ humidity-to-location map:
         ans = min(new_inputs)
         inputs = new_inputs
 
-    print(ans)
-    # submit(ans, year, day, level)
+    submit(ans, year, day, level)
