83a8dce284b11bd790ab90c0492869a1a6637400
Wes Barnett <wes@barnettphd.com>
2023-12-09 21:02:16 -0500

    simpler

--- 9/part2.py
+++ 9/part2.py
@@ -1,6 +1,3 @@
-from functools import reduce
-from operator import sub
-
 from aoc_tool import get_input, submit
 
 
@@ -8,17 +5,15 @@ if __name__ == "__main__":
     year, day, level = 2023, 9, 2
     aoc_input = get_input(year, day)
     lines = aoc_input.splitlines()
+
     ans = 0
     for line in lines:
-        first = []
-        items = [int(x) for x in line.split()]
-        first.append(items[0])
+        last = []
+        items = [int(x) for x in reversed(line.split())]
+        last.append(items[-1])
         while not all(x == 0 for x in items):
             items = [j - i for i, j in zip(items[:-1], items[1:])]
-            first.append(items[0])
-        x = first.pop()
-        while first:
-            x = first.pop() - x
-        ans += x
+            last.append(items[-1])
+        ans += sum(last)
 
     submit(ans, year, day, level)
